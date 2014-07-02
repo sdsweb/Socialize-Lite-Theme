7b6efa3a72f1a998a87dcb9a2df9eaaf771f1c51
scottsousa <scott@scottjsousa.com>
2014-07-02 11:46:27 -0400

    Version 1.2.3
    - Updated to latest version of SDS Core
    - Updated CSS to accommodate new HTML markup

--- functions.php
+++ functions.php
@@ -13,6 +13,7 @@ include_once get_template_directory() . '/includes/class-tgm-plugin-activation.p
 
 include_once get_template_directory() . '/includes/theme-options.php'; // SDS Theme Options
 include_once get_template_directory() . '/includes/theme-functions.php'; // SDS Theme Options Functions
+include_once get_template_directory() . '/includes/class-customize-us-control.php'; // Customize Controller
 include_once get_template_directory() . '/includes/widget-social-media.php'; // SDS Social Media Widget
 
 
@@ -21,12 +22,6 @@ include_once get_template_directory() . '/includes/widget-social-media.php'; // 
  * Theme Specifics
  * ---------------
  */
-
-/**
- * Set the Content Width for embeded items.
- */
-if ( ! isset( $content_width ) )
-	$content_width = 732;
 
 /**
  * This function registers all color schemes available in this theme.
@@ -38,12 +33,16 @@ if ( ! function_exists( 'sds_color_schemes' ) ) {
 				'label' => __( 'Default', 'socialize' ), // Label on options panel (required)
 				'stylesheet' => false, // Stylesheet URL, relative to theme directory (required)
 				'preview' => '#949494', // Preview color on options panel (required)
+				'content_color' => '#363636', // Default content color (required)
+				'background_color' => '#e0e0e0', // Default background color (optional)
 				'default' => true
 			),
 			'slocum-blue' => array(
 				'label' => __( 'Slocum Blue', 'socialize' ),
 				'stylesheet' => '/css/slocum-blue.css',
 				'preview' => '#3c639a',
+				'content_color' => '#535e78',
+				'background_color' => '#e5eef6',
 				'deps' => 'socialize'
 			)
 		);
@@ -181,13 +180,32 @@ if ( ! function_exists( 'soc_body_class' ) ) {
 	}
 }
 
+/**
+ * This function adds the custom Theme Customizer styles to the <head> tag.
+ */
+if ( ! function_exists( 'soc_wp_head' ) ) {
+	add_filter( 'wp_head', 'soc_wp_head', 20 );
+
+	function soc_wp_head() {
+		$sds_theme_options_instance = SDS_Theme_Options_Instance();
+	?>
+		<style type="text/css" id="<?php echo $sds_theme_options_instance->get_parent_theme()->get_template(); ?>-theme-customizer">
+			/* Content Color */
+			.post, .post-meta, #post-author {
+				color: <?php echo get_theme_mod( 'content_color' ); ?>;
+			}
+		</style>
+	<?php
+	}
+}
+
 if ( ! function_exists( 'sds_theme_options_ads' ) ) {
 	add_action( 'sds_theme_options_ads', 'sds_theme_options_ads' );
 
 	function sds_theme_options_ads() {
 		?>
 		<div class="sds-theme-options-ad">
-			<a href="https://slocumthemes.com/wordpress-themes/socialize-pro/" target="_blank" class="sds-theme-options-upgrade-ad">
+			<a href="<?php echo esc_url( sds_get_pro_link( 'theme-options-ad' ) ); ?>" target="_blank" class="sds-theme-options-upgrade-ad">
 				<h3><?php _e( 'Upgrade to Socialize Pro!', 'socialize' ); ?></h3>
 				<ul>
 					<li><?php _e( 'Priority Ticketing Support', 'socialize' ); ?></li>
@@ -211,22 +229,50 @@ if ( ! function_exists( 'sds_theme_options_upgrade_cta' ) ) {
 	function sds_theme_options_upgrade_cta( $type ) {
 		switch( $type ) :
 			case 'color-schemes':
-				?>
-				<p><?php printf( __( '%1$s and receive more color schemes!', 'socialize' ), '<a href="https://slocumthemes.com/wordpress-themes/socialize-pro/" target="_blank">Upgrade to Socialize Pro</a>' ); ?></p>
-				<?php
-				break;
+		?>
+				<p>
+					<?php
+						printf( '<a href="%1$s" target="_blank">%2$s</a> %3$s',
+							esc_url( sds_get_pro_link( 'theme-options-colors' ) ),
+							__( 'Upgrade to Socialize Pro', 'socialize' ),
+							__( 'and receive more color schemes!', 'socialize' )
+						);
+					?>
+				</p>
+		<?php
+			break;
 			case 'web-fonts':
-				?>
-				<p><?php printf( __( '%1$s to use more web fonts!', 'socialize' ), '<a href="https://slocumthemes.com/wordpress-themes/socialize-pro/" target="_blank">Upgrade to Socialize Pro</a>' ); ?></p>
-				<?php
-				break;
+		?>
+				<p>
+					<?php
+						printf( '<a href="%1$s" target="_blank">%2$s</a> %3$s',
+							esc_url( sds_get_pro_link( 'theme-options-fonts' ) ),
+							__( 'Upgrade to Socialize Pro', 'socialize' ),
+							__( 'to use more web fonts!', 'socialize' )
+						);
+					?>
+				</p>
+		<?php
+			break;
 			case 'help-support':
-				?>
-				<p><?php printf( __( '%1$s to receive priority ticketing support!', 'socialize' ), '<a href="https://slocumthemes.com/wordpress-themes/socialize-pro/" target="_blank">Upgrade to Socialize Pro</a>' ); ?></p>
-				<?php
-				break;
+		?>
+				<p>
+					<?php
+						printf( '<a href="%1$s" target="_blank">%2$s</a> %3$s',
+							esc_url( sds_get_pro_link( 'theme-options-help' ) ),
+							__( 'Upgrade to Socialize Pro', 'socialize' ),
+							__( 'to receive priority ticketing support!', 'socialize' )
+						);
+					?>
+				</p>
+		<?php
+			break;
 		endswitch;
 	}
+}
+
+function sds_get_pro_link( $content ) {
+	return esc_url( 'https://slocumthemes.com/wordpress-themes/socialize-pro/?utm_source=socialize&utm_medium=link&utm_content=' . urlencode( sanitize_title_with_dashes( $content ) ) . '&utm_campaign=pro#purchase-theme' );
 }
 
 if ( ! function_exists( 'sds_theme_options_help_support_tab_content' ) ) {
--- readme.txt
+++ readme.txt
@@ -1,89 +1,47 @@
-Changelog
-===
+/**
+ * Font Awesome Web Font - http://fontawesome.io/
+ * License: GPL Compatible (http://fontawesome.io/license/)
+ * Font License: SIL OFL 1.1 (http://scripts.sil.org/OFL)
+ * Code License: MIT License (http://opensource.org/licenses/mit-license.html)
+ * Copyright: Dave Gandy, https://twitter.com/davegandy
+ */
 
-1.1.6
-- Added readme/changelog
+/**
+ * WordPress Core CSS - http://codex.wordpress.org/CSS#WordPress_Generated_Classes
+ * License: GPL2
+ * Copyright: WordPress.org, http://wordpress.org
+ */
+ 
+ /**
+ * CSS Reset - http://meyerweb.com/eric/tools/css/reset/
+ * License: None (public domain)
+ * Copyright: Eric Meyer, http://meyerweb.com/eric/
+ *
+ * We've used Eric Meyer's reset as a base and modified it a bit to suit our needs.
+ */
 
-1.1.5
-- Initial release on wordpress[dot]org
+/**
+ * HTML5 Shiv - https://code.google.com/p/html5shiv/
+ * License: Dual licensed under the MIT or GPL Version 2 licenses
+ * Copyright: Alexander Farkas, http://protofunc.com/
+ */
 
-1.1.3 (Adjusting version number)
----
-- Updated SDS Core (Theme Options)
-- Added all content layout options as a function
-- Added theme styles for content layouts
-- Added featured image size adjustments based on content layout option
-- Added functionality to adjust content width based on content layout option
+/**
+ * WordPress 3.5 Media Uploader Script/Tutorial - http://mikejolley.com/2012/12/using-the-new-wordpress-3-5-media-uploader-in-plugins/
+ * License: None (public domain)
+ * Copyright: Mike Jolley, http://mikejolley.com/
+ *
+ * We've modified this to suit our needs.
+ */
 
-1.1.1
----
-- Fixed issue with Gravity Forms confirmation hook - was breaking page/re-direct confirmation functionality
-- Checking to verify Gravity Forms cssClass isset in our hooks (fixes PHP warning)
-- Updated SDS Core (Theme Options)
-- Made soc_mobile_menu() pluggable
-- Added option to specify whether or not child pages should be shown on mobile menu
+/**
+ * Menu Icon Image - /images/menu-icon-large.png
+ * License: GPL2
+ * Copyright: Slocum Design Studio, http://slocumstudio.com/
+ */
 
-1.1.0
----
-- Updated SDS Core (Theme Options)
-- Updated tags
-
-1.0.9
----
-- Updated SDS Core (Theme Options)
-- Switched child theme functionality check to use is_child_theme()
-- Fixed issue with translation strings (wrong domain name was used)
-- Fixed issue where full width content pages were not 100% width when in responsive view
-
-1.0.8
----
-- Updated to latest version of SDS Core (includes new Toolbar menu for Theme Options)
-- Removed attribution to Nav Walker Class
-- Updated call to stylesheets: parent stylesheet is always loaded and a child theme stylesheet is now enqueued if a child theme is enabled
-
-1.0.7
----
-- Added default styling for all form elements
-- Fixed issue where checkboxes and radio buttons were not appearing on front end
-- Added styles for MailChimp Gravity Form CTA for Newsletters using .mc-gravity, .mc_gravity, .mc-newsletter, .mc_newsletter classes
-- Removed most references to slocumstudio.com
-
-1.0.6
----
-- Updated to latest SDS core (Theme Options)
-- Fixed issue with color scheme dropdown menu colors not appearing
-- Fixed issue where fallback menu items were not shown in mobile menu when no Primary Nav was selected
-- Fixed issue where footer was not styled properly when a color scheme was selected
-- Added filter for developers to adjust default primary nav fallback label
-- Adjusted margin on bottom of content paragraphs to be smaller
-- Fixed issue with search widget title (had -10px margin on top before, removed margins entirely)
-- Fixed issue with bottom margin on all heading elements
-
-1.0.5
----
-- Fixed issue where leave a reply form would not appear due to conditional
-- Fixed styling issue where borders would appear in comments section when comments were disabled on a post/page
-
-1.0.3
----
-- Fixed issue where comment template borders were displayed when comments were disabled on a page
-- Fixed issue with pagination borders appearing when no pagination existed
-
-1.0.2
----
-- Added max-width for iframes
-- Fixed bug with search widget title (was too wide previously)
-
-1.0.1
----
-- Fixed $content_width value for embeds
-- Adjusted with of logo box and header widget container when window was above 1000px
-- Updated links on slocum blue to ensure they were bolded to stand out from content
-- Fixed search box on 404 template (and in general when inserted elswhere other than widgets)
-- Fixed size of post title on responsive view
-- Removed dropdown walker nav class
-- Removed the_excerpt and replaced with the_content, removed "more link" from output using `the_content_more_link` filter
-
-1.0
----
-- New Theme
+/**
+ * Quote Icon Image - /images/quote.png
+ * License: GPL2
+ * Copyright: Slocum Design Studio, http://slocumstudio.com/
+ */
